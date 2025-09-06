432eb6471bb71b2ca67f53a2fa4b3aba1a043207
Hamzah Ullah <hamzahullah@yahoo.com>
2025-09-05 22:32:22 -0400

    chore: setup mongo and kafka

--- CA0/demo/smoke-test.sh
+++ CA0/demo/smoke-test.sh
@@ -17,16 +17,16 @@ echo "== VM4 (Producers) =="
 echo "== Check counts on VM2 =="
 # ssh ubuntu@10.0.1.11 'cd gpu-token-analytics-pipeline/CA0/vm2-mongo && make stats'
 =======
-# ssh ubuntu@10.0.1.10 'cd CS5287/CA0/vm1-kafka && make up topics && make list-topics'
+# ssh ubuntu@10.0.1.10 'cd gpu-token-analytics-pipeline/CA0/vm1-kafka && make up topics && make list-topics'
 
 echo "== VM2 (Mongo) =="
-# ssh ubuntu@10.0.1.11 'cd CS5287/CA0/vm2-mongo && make up && sleep 3 && make init-indexes'
+# ssh ubuntu@10.0.1.11 'cd gpu-token-analytics-pipeline/CA0/vm2-mongo && make up && sleep 3 && make init-indexes'
 
 echo "== VM3 (Processor) =="
-# ssh ubuntu@10.0.1.12 'cd CS5287/CA0/vm3-processor && make build up && sleep 2 && make health'
+# ssh ubuntu@10.0.1.12 'cd gpu-token-analytics-pipeline/CA0/vm3-processor && make build up && sleep 2 && make health'
 
 echo "== VM4 (Producers) =="
-# ssh ubuntu@10.0.1.13 'cd CS5287/CA0/vm4-producers && make build run-once'
+# ssh ubuntu@10.0.1.13 'cd gpu-token-analytics-pipeline/CA0/vm4-producers && make build run-once'
 
 echo "== Check counts on VM2 =="
-# ssh ubuntu@10.0.1.11 'cd CS5287/CA0/vm2-mongo && make stats'
+# ssh ubuntu@10.0.1.11 'cd gpu-token-analytics-pipeline/CA0/vm2-mongo && make stats'
