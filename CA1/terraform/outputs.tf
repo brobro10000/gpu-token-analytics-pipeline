aa86527e808082f38248780037819783c7a99ba6
Hamzah Ullah <hamzahullah@yahoo.com>
2025-09-21 15:05:20 -0400

    feat: connect to aws via terraform (#23)
    * feat: connect to aws via terraform
    
    * chore: organize and add conversation summary

---
+++ CA1/terraform/outputs.tf
@@ -0,0 +1,9 @@
+output "account_id" {
+  value = data.aws_caller_identity.me.account_id
+}
+output "arn" {
+  value = data.aws_caller_identity.me.arn
+}
+output "region" {
+  value = data.aws_region.current.name
+}
