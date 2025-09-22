66a7cb3250627136fe52e4a18c97981e339a93e3
Hamzah Ullah <hamzahullah@yahoo.com>
2025-09-21 20:00:16 -0400

    feat: add kafka templates (#25)
    * feat: add kafka templates
    
    * chore: converation summary

--- CA1/terraform/modules/vpc/main.tf
+++ CA1/terraform/modules/vpc/main.tf
@@ -1,4 +1,6 @@
-locals { tags = { Project = var.name } }
+locals {
+  tags = { Project = var.name }
+}
 
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
@@ -14,7 +16,36 @@ resource "aws_subnet" "this" {
   tags                    = merge(local.tags, { Name = "${var.name}-subnet" })
 }
 
-# (IGW/route table here if public_subnet=true)
+# Create IGW + public route only when public_subnet=true
+resource "aws_internet_gateway" "this" {
+  count  = var.public_subnet ? 1 : 0
+  vpc_id = aws_vpc.this.id
+  tags   = merge(local.tags, { Name = "${var.name}-igw" })
+}
 
-output "vpc_id" { value = aws_vpc.this.id }
-output "subnet_id" { value = aws_subnet.this.id }
+resource "aws_route_table" "public" {
+  count  = var.public_subnet ? 1 : 0
+  vpc_id = aws_vpc.this.id
+  tags   = merge(local.tags, { Name = "${var.name}-public-rt" })
+}
+
+resource "aws_route" "public_internet_access" {
+  count                  = var.public_subnet ? 1 : 0
+  route_table_id         = aws_route_table.public[0].id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this[0].id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = var.public_subnet ? 1 : 0
+  subnet_id      = aws_subnet.this.id
+  route_table_id = aws_route_table.public[0].id
+}
+
+output "vpc_id" {
+  value = aws_vpc.this.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.this.id
+}
